1369c635fd33fee4e099afa8a81c11f0b8e1ca8a
Bernardo <dinhomcardozo@gmail.com>
2024-11-24 21:45:50 -0300

    Atualiza um article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,44 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all #uma variavel artigos com @ agora é uma variavel publica para a view e mostra TODOS os artigos
+  def index # O método index pega todos os artigos do banco e os envia para a visão.
+    @articles = Article.all # Uma variável artigos com @ agora é uma variável pública para a view e mostra TODOS os artigos
   end
 
-  def show # criado antes no routes config\routes.rb
-    @article = Article.find(params[:id]) #cria uma variavel e puxa do Model Article e procura pelo ID no caso do artigo
+  def show #Mostrar
+    @article = Article.find(params[:id]) # Cria uma variável e puxa do Model Article e procura pelo ID no caso do artigo
+  end
+
+  def new #novo
+    @article = Article.new # Cria um artigo vazio
+  end
+
+  def create #no momento da criação de um novo artigo
+    @article = Article.new(article_params) # Cria um novo artigo com os parâmetros definidos
+
+    if @article.save # Se salvar com sucesso
+      redirect_to @article # Redireciona para o artigo recém-criado (no caso, a página show)
+    else
+      render :new # Se houver erro, renderiza a página de novo (mantém os dados preenchidos)
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id]) #sem precisar declarar, ele já puxa o conteúdo para editar
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params) # Se salvar com sucesso os parametros permitidos
+      redirect_to @article # Redireciona para o artigo recém-criado (no caso, a página show)
+    else
+      render :edit # Se houver erro, renderiza a página de edição (mantém os dados preenchidos)
+    end
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body) # Só permite os parâmetros title e body para garantir segurança
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5 }   #para validar que nada está em branco(vazio) que é obrigatório e com minimo 5 caracteres
+    validates :body, presence: true, length: { minimum: 10 }
 end
